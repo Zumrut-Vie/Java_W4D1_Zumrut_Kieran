a12eeb5b44f08124bd29ea52b16e4afbac433016
Zumrut-Vie <ekinci.z@gmail.com>
2020-07-20 10:43:31 +0200

    infinity

--- Zumrut/DebugDemo1.java
+++ Zumrut/DebugDemo1.java
@@ -13,6 +13,7 @@ public class DebugDemo1
   public static void calculateTip(double bill)
   {
       final double rate = 2; // Consider these are percentage
+      money = bill1;
       double tip;
       tip = money * rate;
       System.out.println("Tip: " + tip);
